db65d986a2c0313be352bfee5bbb76ff57e360cd
dongsulee <dongsulee@nextedition.co.kr>
2023-10-18 16:34:48 +0900

    feat : boilerplate code

---
+++ index.js
@@ -0,0 +1,34 @@
+const render = () => {
+  const $button = document.getElementById("button");
+  $button.addEventListener("click", () => {
+    onClick();
+  });
+};
+
+render();
+
+const apiRequest = (request) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error(`${request} 에러!`));
+    }, 1000);
+  });
+};
+
+const onClick = async () => {
+  const requests = [
+    "최근 배송지 주소 가져오기",
+    "사용가능한 적립금 가져오기",
+    "주문 반품 환불 내역 가져오기",
+  ];
+  const promises = requests.map((request) => apiRequest(request));
+  const results = await Promise.allSettled(promises);
+
+  results.forEach((result) => {
+    const { status, reason } = result;
+
+    if (status === "rejected") {
+      alert(reason);
+    }
+  });
+};
